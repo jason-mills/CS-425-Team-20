91ae04c3accf43b588f3085b59f08136a2c16090
jason-mills <jasonmills@nevada.unr.edu>
2023-02-09 16:02:18 -0800

    Open3d Rendering and Processing Progress

---
+++ jason-mills/Open3D/render.py
@@ -0,0 +1,8 @@
+import open3d as o3d
+import numpy as np
+
+def main():
+    print("something")
+
+if __name__ == '__main__':
+    main()
---
+++ jason-mills/Open3D/scan.py
@@ -0,0 +1,8 @@
+import open3d as o3d
+
+
+def main():
+    print("something")
+
+if __name__ == '__main__':
+    main()
