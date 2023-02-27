a36ebe710e69769c0fa658e6135edf4057e680dc
Froilan Luna <froil@DESKTOP-750FS49.localdomain>
2023-02-26 17:02:35 -0800

    Added GUI to remote repository

---
+++ C3PO/C3PO/Model/ScannerComponent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C3PO.Model
+{
+    internal class ScannerComponent : IComponent
+    {
+        public Task<int> StartOpAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> StopOpAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
