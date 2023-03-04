75a5a5748abaa1d59fc254fccfa00789b3c177c7
Froilan Luna <froil@DESKTOP-750FS49.localdomain>
2023-03-03 16:11:13 -0800

    Added functionality for calling scan program

---
+++ C3PO/C3PO/C3PO/Stores/NavigationStore.cs
@@ -0,0 +1,46 @@
+﻿using C3PO.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C3PO.Stores
+{
+    public class NavigationStore
+    {
+        private ViewModelBase _currentViewModel;
+
+        public event Action CurrentViewModelChanged;
+
+        public ViewModelBase CurrentViewModel
+        {
+            get
+            {
+                return _currentViewModel;
+            }
+            set
+            {
+                _currentViewModel = value;
+                OnCurrentViewModelChanged();
+            }
+        }
+        private ViewModelBase _previousViewModel;
+        public ViewModelBase PreviousViewModel
+        {
+            get
+            {
+                return _previousViewModel;
+            }
+            set
+            {
+                _previousViewModel = value;
+            }
+        }
+
+        private void OnCurrentViewModelChanged()
+        {
+            CurrentViewModelChanged?.Invoke();
+        }
+    }
+}
