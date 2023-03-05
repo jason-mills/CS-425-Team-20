ef011db5ea338acac5c8373cad995a0ba4f8919f
Froilan Luna <froil@DESKTOP-750FS49.localdomain>
2023-03-05 14:09:18 -0800

    Added beta open 3d viewer

--- C3PO/C3PO/C3PO/Model/ComponentLinker.cs
+++ C3PO/C3PO/C3PO/Model/ComponentLinker.cs
@@ -1,6 +1,7 @@
 ﻿using C3PO.ViewModel;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Diagnostics;
 using System.Linq;
 using System.Management.Automation.Language;
@@ -13,43 +14,67 @@ namespace C3PO.Model
 {
     class ComponentLinker
     {
-        private IComponent _reconstructComp;
-        private IComponent _scanComp;
+        private readonly IComponent _reconstructComp;
+        private readonly IComponent _scanComp;
+        private DateTime _startTime;
+        private string _endTimeStr;
+        public string EndTimeStr
+        {
+            get
+            {
+                return _endTimeStr;
+            }
+            private set
+            {
+                _endTimeStr = value;
+            }
+        }
 
         /* Constructors */
         public ComponentLinker()
         {
             _reconstructComp = new ReconstructionComponent();
             _scanComp = new ScannerComponent();
+
+            _startTime = new DateTime();
+            _endTimeStr = "";
         }
 
         public bool StartScan()
         {
-            // Declaring and configuring process-running object
-            var p = new Process()
-            {
-                StartInfo = new ProcessStartInfo()
-                {
-                    FileName = "C:\\dev\\CS-425-Team-20\\jason-mills\\Scanning\\x64\\Debug\\Scanning.exe"
-                }
-            };
+            //// Declaring and configuring process-running object
+            //var p = new Process()
+            //{
+            //    StartInfo = new ProcessStartInfo()
+            //    {
+            //        FileName = "C:\\dev\\CS-425-Team-20\\jason-mills\\Scanning\\x64\\Debug\\Scanning.exe"
+            //    }
+            //};
 
-            // Start process
-            p.Start();
-            p.WaitForExit();
-
+            //// Start process
+            //p.Start();
+            //p.WaitForExit();
+            _startTime = DateTime.Now;
             return true;
         }
 
         public bool StartReconstruction()
         {
-            // Declaring and configuring process-running object
+            //// Declaring and configuring process-running object
+            //var p = new Process()
+            //{
+            //    StartInfo = new ProcessStartInfo()
+            //    {
+            //        FileName = "C:\\dev\\CS-425-Team-20\\jason-mills\\reconstruct.exe",
+            //        Arguments = "--dir=C:\\dev\\CS-425-Team-20\\jason-mills\\Scanning\\Scanning\\src\\scans\\ --prefix=bun --out=C:\\Users\\froil\\Downloads\\bunny\\data\\out.ply"
+            //    }
+            //};
             var p = new Process()
             {
                 StartInfo = new ProcessStartInfo()
                 {
-                    FileName = "C:\\dev\\CS-425-Team-20\\jason-mills\\reconstruct.exe",
-                    Arguments = "--dir=C:\\dev\\CS-425-Team-20\\jason-mills\\Scanning\\Scanning\\src\\scans\\ --prefix=bun --out=C:\\Users\\froil\\Downloads\\bunny\\data\\out.ply"
+                    FileName = "C:\\Users\\froil\\source\\repos\\PointCloudReconstruction\\Debug\\reconstruct.exe",
+                    Arguments = "--dir=C:\\Users\\froil\\Downloads\\bunny\\data --prefix=bun --out=C:\\Users\\froil\\Downloads\\bunny\\data\\out.ply"
                 }
             };
 
@@ -62,7 +87,13 @@ namespace C3PO.Model
 
         public bool Finish()
         {
+            _endTimeStr = (DateTime.Now - _startTime).ToString();
             return true;
+        }
+
+        public String GetTimeSpanned()
+        {
+            return _endTimeStr;
         }
     }
 }
---
+++ C3PO/C3PO/C3PO/ViewModel/Commands/ViewResultsCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C3PO.ViewModel.Commands
+{
+    internal class ViewResultsCommand : CommandBase
+    {
+        public ViewResultsCommand() { }
+
+        public override void Execute(object? parameter)
+        {
+            // Declaring and configuring process-running object
+            var p = new Process()
+            {
+                StartInfo = new ProcessStartInfo()
+                {
+                    FileName = "C:\\dev\\vcpkg\\packages\\python3_x64-windows\\tools\\python3\\python.exe ",
+                    Arguments = "C:\\Users\\froil\\source\\repos\\CS-425-Team-20\\jason-mills\\Open3D\\render.py " + @"""C:\\Users\\froil\\source\\repos\\CS-425-Team-20\\jason-mills\\Open3D\\PLY Files\\bowl_1_0.ply""",
+                }
+            };
+
+            // Start process
+            p.Start();
+            p.WaitForExit();
+        }
+    }
+}
