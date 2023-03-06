0db4806377df112db7011b8451a7826f4fd8fbbd
Froilan Luna <froil@DESKTOP-750FS49.localdomain>
2023-03-05 16:33:54 -0800

    Added beta xyzToPly functionality

--- C3PO/C3PO/Model/ComponentLinker.cs
+++ C3PO/C3PO/Model/ComponentLinker.cs
@@ -1,6 +1,7 @@
 ﻿using C3PO.ViewModel;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Diagnostics;
 using System.Linq;
 using System.Management.Automation.Language;
@@ -13,33 +14,85 @@ namespace C3PO.Model
 {
     class ComponentLinker
     {
-        private IComponent _reconstructComp;
-        private IComponent _scanComp;
+        private readonly IComponent _reconstructComp;
+        private readonly IComponent _scanComp;
+        private DateTime _startTime;
+        private string _endTimeStr;
+        public string EndTimeStr
+        {
+            get
+            {
+                return _endTimeStr;
+            }
+            private set
+            {
+                _endTimeStr = value;
+            }
+        }
 
         /* Constructors */
         public ComponentLinker()
         {
             _reconstructComp = new ReconstructionComponent();
             _scanComp = new ScannerComponent();
+
+            _startTime = new DateTime();
+            _endTimeStr = "";
         }
 
         public bool StartScan()
         {
-            // Call hardware software to start scan
+            _startTime = DateTime.Now;
+            // Declaring and configuring process-running object
+            string path = System.IO.Directory.GetCurrentDirectory();
+            var p = new Process()
+            {
+                StartInfo = new ProcessStartInfo()
+                {
+                    FileName = path + "\\bin\\Scanning.exe"
+                }
+            };
+
+            //// Start process
+            p.Start();
+            p.WaitForExit();
+
+            p = new Process()
+            {
+                StartInfo = new ProcessStartInfo()
+                {
+                    FileName = "python.exe",
+                    Arguments = path + "\\bin\\xyzToPly.py " + path + "\\output " + " temp " + path + "\\output out"
+                }
+            };
+
+            //// Start process
+            p.Start();
+            p.WaitForExit();
+
             return true;
         }
 
         public bool StartReconstruction()
         {
             // Declaring and configuring process-running object
+            string path = System.IO.Directory.GetCurrentDirectory();
             var p = new Process()
             {
                 StartInfo = new ProcessStartInfo()
                 {
-                    FileName = "C:\\Users\\froil\\source\\repos\\C3PO\\C3PO\\Resources\\bin\\reconstruct.exe",
-                    Arguments = "--dir=C:\\Users\\froil\\Downloads\\bunny\\data --prefix=bun --out=C:\\Users\\froil\\Downloads\\bunny\\data\\out.ply"
+                    FileName = "\\bin\\reconstruct.exe",
+                    Arguments = "--dir=" + path + "\\output --prefix=out --out=" + path + "\\output\\final.ply"
                 }
             };
+            //var p = new Process()
+            //{
+            //    StartInfo = new ProcessStartInfo()
+            //    {
+            //        FileName = "C:\\Users\\froil\\source\\repos\\PointCloudReconstruction\\Debug\\reconstruct.exe",
+            //        Arguments = "--dir=C:\\Users\\froil\\Downloads\\bunny\\data --prefix=bun --out=C:\\Users\\froil\\Downloads\\bunny\\data\\out.ply"
+            //    }
+            //};
 
             // Start process
             p.Start();
@@ -50,7 +103,13 @@ namespace C3PO.Model
 
         public bool Finish()
         {
+            _endTimeStr = (DateTime.Now - _startTime).ToString();
             return true;
+        }
+
+        public String GetTimeSpanned()
+        {
+            return _endTimeStr;
         }
     }
 }
