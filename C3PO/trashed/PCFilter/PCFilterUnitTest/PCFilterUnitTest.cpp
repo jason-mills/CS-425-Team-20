375cabc4d33edacca2b382736de2b6e11b8cbe4f
Froilan Luna <froil@DESKTOP-750FS49.localdomain>
2023-05-09 19:43:05 -0700

    Added trashed source code

---
+++ C3PO/trashed/PCFilter/PCFilterUnitTest/PCFilterUnitTest.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace PCFilterUnitTest
+{
+	TEST_CLASS(PCFilterUnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
